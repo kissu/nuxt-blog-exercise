29707d8a58ad6db3f7d5af0c9c99bc69dec68038
Konstantin BIFERT <hello@kissu.io>
2018-04-15 21:38:44 +0200

    Add basic files & some styles

---
+++ pages/posts/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .posts-page
+    h2 Awesome posts
+</template>
+
+<style scoped>
+  .posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
