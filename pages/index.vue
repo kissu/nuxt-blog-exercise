c3743f39c7fc56e0954ecc7e284327547e833983
Konstantin BIFERT <hello@kissu.io>
2018-04-15 22:01:13 +0200

    Add basic structure + style for the posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,95 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt-blog-exercise
-      </h1>
-      <h2 class="subtitle">
-        My fabulous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  .home-page
+    section.intro
+      h1 Hello folks !
+    section.featured-posts
+      nuxt-link.post-preview(:to="'/posts/' + 1")
+        article
+          .post-thumbnail(style="background-image: url('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-604121.jpg')")
+          .post-content
+            h1 Post title
+            p Preview text
+      nuxt-link.post-preview(:to="'/posts/' + 2")
+        article
+          .post-thumbnail(style="background-image: url('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-20179.jpg')")
+          .post-content
+            h1 Post title
+            p Preview text
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+<style scoped>
+  .intro {
+    height: 300px;
+    position: relative;
+    padding: 30px;
+    box-sizing: border-box;
+    background-position: center;
+    background-size: cover;
   }
-}
-</script>
 
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
+  .intro h1 {
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    width: 90%;
+    font-size: 1.5rem;
+    color: black;
+    background-color: rgb(211, 211, 211);
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px black;
+    box-sizing: border-box;
+    border: 1px solid black;
+  }
+
+  @media (min-width: 768px) {
+    .intro h1 {
+      font-size: 2rem;
+    }
+  }
+
+  .featured-posts {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .post-preview {
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #ccc;
+    background-color: white;
+    width: 90%;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  @media (min-width: 850px) {
+    .post-preview {
+      width: 400px;
+      margin: 10px;
+    }
+  }
+
+  .post-thumbnail {
+    width: 100%;
+    height: 200px;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .post-content {
+    padding: 10px;
+    text-align: center;
+  }
+
+  a:hover .post-content,
+  a:active .post-content {
+    background-color: #ccc;
+  }
 </style>
