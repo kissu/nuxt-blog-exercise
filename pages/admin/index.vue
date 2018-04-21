1942e3ff96508d55a1956147cf2a76ecbeca3728
Konstantin BIFERT <hello@kissu.io>
2018-04-21 22:46:59 +0200

    Add CRUD draft

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,3 +1,34 @@
 <template lang="pug">
-  div This is the admin page
+  .admin-page
+    section.new-post
+      button(@click="$router.push('/admin/new-post')") Create post
+    section.existing-posts
+      h1 Existing posts
+      post-list
 </template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+
+export default {
+  components: {
+    PostList
+  }
+}
+</script>
+
+<style scoped>
+  .admin-page {
+    padding: 20px;
+  }
+
+  .new-post {
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+  }
+
+  .existing-posts h1 {
+    text-align: center;
+  }
+</style>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  .admin-new-post-page
+    section.new-post-form
+      form(@submit.prevent="onSave")
+        app-control-input(v-model="editedPost.author") Author Name
+        app-control-input(v-model="editedPost.title") Title
+
+        app-control-input(v-model="editedPost.thumbnailLink") Thumbnail Link
+        app-control-input(control-type="textarea" v-model="editedPost.content") Content
+
+        app-button(type="submit") Save
+        app-button(type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel") Cancel
+
+</template>
+
+<script>
+import AppControlInput from '@/components/UI/AppControlInput'
+import AppButton from '@/components/UI/AppButton'
+
+export default {
+  data() {
+    return {
+      editedPost: {
+        author: '',
+        title: '',
+        thumbnailLink: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    onSave() {
+      console.log(this.editedPost)
+    },
+    onCancel() {
+      this.$router.push('/admin')
+    }
+  },
+  components: {
+    AppControlInput,
+    AppButton
+  }
+}
+</script>
