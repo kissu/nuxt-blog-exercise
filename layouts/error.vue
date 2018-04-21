d8f846150ddc9a91865848cd559bc2e2bde142b4
Konstantin BIFERT <hello@kissu.io>
2018-04-21 23:49:53 +0200

    Add the error page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+  .error-page
+    h1 Oops, something went wrong!
+    p Back to
+      a(href="/")  safety !
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
