9141813a9365195e284335bff62fd1cdd7f875d1
Konstantin BIFERT <hello@kissu.io>
2018-04-21 17:11:07 +0200

    Implement the top navbar and sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
-<template>
-  <div>
-    <nuxt/>
-  </div>
+<template lang='pug'>
+  div
+    TheHeader(@sidenavToggle="displaySidenav = !displaySidenav")
+    TheSidenav(:show="displaySidenav" @close="displaySidenav = false")
+    nuxt
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
+
+export default {
+  data() {
+    return {
+      displaySidenav: false
+    }
+  },
+  components: {
+    TheHeader,
+    TheSidenav
+  }
+}
+</script>
+
 
 <style>
 html {
