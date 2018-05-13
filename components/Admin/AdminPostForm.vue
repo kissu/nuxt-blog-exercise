cde0d9c506ee2a4ca5c742bc573fbc0c3035e475
Konstantin BIFERT <hello@kissu.io>
2018-05-13 19:16:54 +0200

    Make the post creation fields intuitive-ier

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -1,11 +1,10 @@
 <template lang="pug">
   form(@submit.prevent="onSave")
-    app-control-input(v-model="editedPost.author") Author Name
+    app-control-input(v-model="editedPost.author") Name
     app-control-input(v-model="editedPost.title") Title
-
+    app-control-input(control-type="textarea" v-model="editedPost.previewText") Motto
+    app-control-input(control-type="textarea" v-model="editedPost.content") Description
     app-control-input(v-model="editedPost.thumbnail") Thumbnail Link
-    app-control-input(control-type="textarea" v-model="editedPost.content") Content
-    app-control-input(control-type="textarea" v-model="editedPost.previewText") Preview text
 
     app-button(type="submit") Save
     app-button(type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel") Cancel
